aec9bf2dfa08f6d97300b34383ee15298be456bd
AmaranthineCodices <git@amaranthinecodices.me>
2018-10-08 12:53:24 -0400

    fix indentation

--- backend/src/requests/set_read.rs
+++ backend/src/requests/set_read.rs
@@ -4,24 +4,24 @@ use protocol::{Response};
 use super::Error;
 
 pub fn set_read(request: SetRead, conn: &mut Connection) -> Result<Response, Error> {
-	let tx = conn.transaction()?;
+    let tx = conn.transaction()?;
 
-	let read_int = if request.is_read {
-		1
-	}
-	else {
-		0
-	};
+    let read_int = if request.is_read {
+        1
+    }
+    else {
+        0
+    };
 
-	{
-		let mut stmt = tx.prepare_cached("UPDATE feedEntries SET isRead = ? WHERE rowid = ?;")?;
+    {
+        let mut stmt = tx.prepare_cached("UPDATE feedEntries SET isRead = ? WHERE rowid = ?;")?;
 
-		for entry_id in request.entry_ids {
-			stmt.execute(&[&entry_id, &read_int])?;
-		}
-	}
+        for entry_id in request.entry_ids {
+            stmt.execute(&[&entry_id, &read_int])?;
+        }
+    }
 
-	tx.commit()?;
+    tx.commit()?;
 
-	Ok(Response::Success)
+    Ok(Response::Success)
 }
